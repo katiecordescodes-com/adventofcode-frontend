371b1be3937dffbf7453e213059db19519a63d6f
Katharine Cordes <katharinegillis@users.noreply.github.com>
2023-01-15 20:40:42 -0700

    AOC-5: Add Jest Testing (#2)
    * feat: add jest testing
    
    * fix: add page extensions so tests are not pages

---
+++ pages/api/__tests__/ping.test.ts
@@ -0,0 +1,16 @@
+import '@testing-library/jest-dom';
+import { createMocks } from 'node-mocks-http';
+import handler from '../ping.api';
+
+describe('/api/ping', () => {
+    it('should return successful', () => {
+        const { req, res } = createMocks({
+            method: 'GET',
+        });
+
+        handler(req, res);
+
+        expect(res._getStatusCode()).toEqual(200);
+        expect(JSON.parse(res._getData())).toEqual('');
+    })
+})
