5ecd3c5e57f4db31ef15fd05ef954cc5c5504046
Katharine Cordes <katharinegillis@users.noreply.github.com>
2023-01-16 23:02:54 -0700

    feat: e2e tests for landing page (#4)
    refs: AOC-6

--- .codeceptjs/step_definitions/landingpage_steps.ts
+++ .codeceptjs/step_definitions/landingpage_steps.ts
@@ -1,9 +1,12 @@
 const { I } = inject();
 
-When(/^I go to the root of the site$/, async () => {
+When(/^I go to the root of the site$/, async (): Promise<void> => {
     await I.amOnPage('/');
 });
 
-Then(/^I see the landing page with the text "([^"]*)"$/, async (text: string) => {
+Then(/^I see the landing page with the text "([^"]*)"$/, async (text: string): Promise<void> => {
     await I.see(text);
+});
+Then(/^I see the landing page with the title "([^"]*)"$/, async (title: string): Promise<void> => {
+    await I.seeTitleEquals(title);
 });
