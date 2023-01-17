c9a7bb36f2fe220581322b6e7d4bf6059eb7bdb6
Katharine Cordes <katharinegillis@users.noreply.github.com>
2023-01-16 19:44:41 -0700

    AOC-8: Setup ESLint (#3)
    * feat: add eslint and fix files
    
    refs: AOC-8
    
    * chore: remove coverage from eslint
    
    refs: AOC-8

--- pages/__tests__/index.test.tsx
+++ pages/__tests__/index.test.tsx
@@ -1,13 +1,14 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
+
 import Home from '../index.page';
 import '@testing-library/jest-dom';
 
 describe('Home', () => {
-    it('renders a heading', () => {
-        render(<Home />);
+  it('renders a heading', () => {
+    render(<Home />);
 
-        const edit = screen.getByText('Get started by editing');
+    const edit = screen.getByText('Get started by editing');
 
-        expect(edit).toBeInTheDocument();
-    });
+    expect(edit).toBeInTheDocument();
+  });
 });
--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,9 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
 
-const inter = Inter({ subsets: ['latin'] })
+import { Inter } from '@next/font/google';
+
+import styles from '../styles/Home.module.css';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -119,5 +121,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
